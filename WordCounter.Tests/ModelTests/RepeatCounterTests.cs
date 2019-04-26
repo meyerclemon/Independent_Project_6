60fbb6b7bf7b1c42181766d6672e0ddfd4a0ec91
Maya Frame <mayacframe@gmail.com>
2019-04-26 12:14:23 -0700

    add successfully tested constructor

--- WordCounter.Tests/ModelTests/RepeatCounterTests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -7,7 +7,11 @@ namespace WordCounter.Tests
     public class WordCounterTest
     {
         [TestMethod]
-        public void WordCounterConstructor_CreatesInstanceOfWordCounter_WordCounter()
+        public void WordCountConstructor_CreateInstanceOfWordCounter_Word()
         {
-          
+          WordCounter newWordCounter = new WordCounter("", "");
+          Assert.AreEqual(typeof(WordCounter), newWordCounter.GetType());
         }
+
+    }
+}
--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -4,5 +4,12 @@ namespace WordCounter
 {
     public class WordCounter
     {
-      
+      private string _word;
+      private string _sentence;
+      public WordCounter(string word, string sentence)
+      {
+        _word = word;
+        _sentence = sentence;        
+      }
     }
+}
