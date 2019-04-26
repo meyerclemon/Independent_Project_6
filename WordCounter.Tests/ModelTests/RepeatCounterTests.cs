6d018ff36f31c5bff070b88a8010c5cbbda3d445
Maya Frame <mayacframe@gmail.com>
2019-04-26 14:23:32 -0700

    actually add setter this time

--- WordCounter.Tests/ModelTests/RepeatCounterTests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -12,15 +12,26 @@ namespace WordCounter.Tests
           WordItem newWordItem = new WordItem("");
           Assert.AreEqual(typeof(WordItem), newWordItem.GetType());
         }
-//All good above
 
         [TestMethod]
-        public void GetWordInput_ReturnsWordInput_String()
+        public void GetWordInput_SetWordInput_String()
         {
           string wordInput = "booyah";
           WordItem newWordItem = new WordItem(wordInput);
           string result = newWordItem.GetWordInput();
           Assert.AreEqual(wordInput, result);
         }
+
+        [TestMethod]
+        public void SetWordInput_SetWordInput_String()
+        {
+          string wordInput = "booyah";
+          WordItem newWordItem = new WordItem(wordInput);
+          string updatedWordInput = "abooya";
+          newWordItem.SetWordInput(updatedWordInput);
+          string result = newWordItem.GetWordInput();
+          Assert.AreEqual(updatedWordInput, result);
+        }
+//All good above
     }
 }
--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -2,19 +2,22 @@ namespace WordCounter.Models
 {
     public class WordItem
     {
-//All good above
+
       private string _wordInput;
 
       public WordItem(string wordInput)
       {
         _wordInput = wordInput;
-
       }
-
       public string GetWordInput()
       {
         return _wordInput;
       }
+      public void SetWordInput(string newWordItem)
+      {
+        _wordInput = newWordItem;
+      }
+//All good above
 
     }
 }
