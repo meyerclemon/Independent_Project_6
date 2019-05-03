2b84b19bf7a87ea888c19941343430b04bb2e286
Maya Frame <mayacframe@gmail.com>
2019-05-03 14:41:07 -0700

    fix layout bug so splash page will load

--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
-using MySql.Data.MySqlClient;
-using WordCounter;
+
 
 namespace WordCounter.Models
 {
--- WordCounter/Startup.cs
+++ WordCounter/Startup.cs
@@ -24,7 +24,7 @@ namespace WordCounter
     }
 
     public void Configure(IApplicationBuilder app)
-    {
+    { app.UseDeveloperExceptionPage();
       app.UseMvc(routes =>
       {
         routes.MapRoute(
